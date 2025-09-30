fd0ddc10e6fa4f26db23e20ed2fb213782fd317b
Raveenthiran Ajan <rjajanajan@gmail.com>
2025-09-30 15:31:45 +0530

    submission error fixed

--- backend/models/assignmentSchema.js
+++ backend/models/assignmentSchema.js
@@ -4,9 +4,9 @@ const assignmentSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   dueDate: { type: Date, required: true },
+  subject: { type: String, required: true },
   teacherId: { type: mongoose.Schema.Types.ObjectId, ref: 'teacher', required: true }, 
   classId: { type: mongoose.Schema.Types.ObjectId, ref: 'sclass', required: true }, 
-  subjectId: { type: mongoose.Schema.Types.ObjectId, ref: 'subject', required: true },
   filePath: { type: String },
   createdAt: { type: Date, default: Date.now }
 });
